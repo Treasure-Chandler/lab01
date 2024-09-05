1039a498d2cbbd2be1ab399ed7a434408a073923
Treasure <tchandler1211@gmail.com>
2024-09-04 20:04:08 -0400

    Created NumericTypes class

---
+++ lab01/NumericTypes.java
@@ -0,0 +1,24 @@
+/*
+ * Treasure Chandler
+ * CS 16000-01 – 02/03, Fall Semester 2024
+ * Lab 01
+ */
+
+ package lab01;
+
+ public class NumericTypes {
+     /**
+      * 
+      * @param args      entered values
+      */
+     public static void main(String[] args) {
+         // variables declaration
+         byte byteNumber;
+         short shortNumber;
+         long longNumber;
+         float floatNumber;
+         double doubleNumber;
+ 
+     } // end of main()
+ } // end of NumericTypes
+ 
