deaf469618747f71ce82b7d0f838e624b026ae06
૮꒰ ˶• ༝ •˶꒱ა ♡ <treasure89238@students.sbcsc.k12.in.us>
2024-09-05 20:21:18 -0400

    Worked halfway through 3(B)

--- lab01/NumericTypes.java
+++ lab01/NumericTypes.java
@@ -4,48 +4,51 @@
  * Lab 01
  */
 
+
  package lab01;
 
- import java.util.Scanner; //needed for the Scanner class 
+
+ import java.util.Scanner; //needed for the Scanner class
+
 
  public class NumericTypes {
-     /**
-      * 
-      * @param args      entered values
-      */
-     public static void main(String[] args) {
-         // variables declaration
-         // problems 1 and 2(A):
-         byte byteNumber = 127; // a literal is a value that is written into
-         short shortNumber = 127; // the code of a program
-         int intNumber = 127;
-         long longNumber = 127;
-         float floatNumber = 127f;
-         double doubleNumber = 127.0;
-         String pbmTitle = "For problem 2(A): "; // string literal
-
-         // problem 2(A):
-         String output = "\n\tbyteNumber = " + byteNumber +
-                         "\n\tshortNumber = " + shortNumber +
-                         "\n\tintNumber = " + intNumber +
-                         "\n\tlongNumber = " + longNumber +
-                         "\n\tfloatNumber = " + floatNumber +
-                         "\n\tdoubleNumber = " + doubleNumber + "\n";
-
+    /**
+     *
+     * @param args      entered values
+     */
+    public static void main(String[] args) {
+        // variables declaration
+        // problems 1 and 2(A):
+        byte byteNumber = 127;
+        short shortNumber = 127;
+        int intNumber = 127;
+        long longNumber = 127;
+        float floatNumber = 127f;
+        double doubleNumber = 127.0;
+        String pbmTitle = "For problem 2(A): ";
+        String output = "\n\tbyteNumber = " + byteNumber +
+                        "\n\tshortNumber = " + shortNumber +
+                        "\n\tintNumber = " + intNumber +
+                        "\n\tlongNumber = " + longNumber +
+                        "\n\tfloatNumber = " + floatNumber +
+                        "\n\tdoubleNumber = " + doubleNumber + "\n";
         System.out.println(pbmTitle + output);
 
-        displayConsole(pbmTitle, byteNumber, shortNumber, intNumber, longNumber, floatNumber, doubleNumber);
+        displayConsole(pbmTitle, byteNumber, shortNumber,
+                      intNumber, longNumber, floatNumber,
+                      doubleNumber);
+
 
         // problem 2(B):
         Scanner keyboardInput = new Scanner(System.in);
-        System.out.println("Please enter the same integer number, 123, six times:");
+        System.out.println("Please enter the same integer number," + 
+                           " 123, six times:");
         byteNumber = keyboardInput.nextByte();
         shortNumber = keyboardInput.nextShort();
         intNumber = keyboardInput.nextInt();
         longNumber = keyboardInput.nextLong();
         floatNumber = keyboardInput.nextFloat();
         doubleNumber = keyboardInput.nextDouble();
-        keyboardInput.close();
 
         pbmTitle = "For problem 2(B): ";
         String secondOutput = "\n\tbyteNumber = " + byteNumber +
@@ -56,30 +59,94 @@
                               "\n\tdoubleNumber = " + doubleNumber + "\n";
         System.out.println(pbmTitle + secondOutput);
 
-        displayConsole(pbmTitle, byteNumber, shortNumber, intNumber, longNumber, floatNumber, doubleNumber);
+        displayConsole(pbmTitle, byteNumber, shortNumber,
+                       intNumber, longNumber, floatNumber,
+                       doubleNumber);
  
-     } // end of main()
+        // problem 3(A):
+        byteNumber = 127;
+        shortNumber = 127;
+        intNumber = 127;
+        longNumber = 127;
+        floatNumber = 127.0f;
+        doubleNumber = 127;
 
-     // problem 2(A):
-     /**
-      * 
-      * @param pbm      title to present the displayed values
-      * @param bN       byte number
-      * @param sN       short number
-      * @param iN       int number
-      * @param fN       float number
-      * @param dN       double number
-      */
-     public static void displayConsole(String pbm, byte bN, short sN, int iN, long lN, float fN, double dN) {
+        pbmTitle = "For problem 3(A): ";
+        String thirdOutput = "\n\tbyteNumber = " + byteNumber +
+                             "\n\tshortNumber = " + shortNumber +
+                             "\n\tintNumber = " + intNumber +
+                             "\n\tlongNumber = " + longNumber +
+                             "\n\tfloatNumber = " + floatNumber +
+                             "\n\tdoubleNumber = " + doubleNumber + "\n";
+        System.out.println(pbmTitle + thirdOutput);
+
+        displayConsole(pbmTitle, byteNumber, shortNumber,
+                       intNumber, longNumber, floatNumber,
+                       doubleNumber);
+        /*
+         * 3(A) Explanation:
+         * The output is correct as all of the literals
+         * display the value of 127. These literals have
+         * gone through changes. They were first
+         * initialized with the value of 127, then their
+         * values would be what the user would input
+         * (for example, 123 from the original prompt),
+         * then their value is changed again back to 127,
+         * and their values of 127 will be printed to the
+         * console. This is mainly because the last value
+         * they are changed will be printed to the console.
+         */
+
+        // problem 3(B):
+        Scanner secondKeyboardInput = new Scanner(System.in);
+        System.out.println("Please enter the same integer number," +
+                           " 127, six times:");
+        byteNumber = secondKeyboardInput.nextByte();
+        shortNumber = secondKeyboardInput.nextShort();
+        intNumber = secondKeyboardInput.nextInt();
+        longNumber = secondKeyboardInput.nextLong();
+        floatNumber = secondKeyboardInput.nextFloat();
+        doubleNumber = secondKeyboardInput.nextDouble();
+
+        pbmTitle = "For problem 3(B): ";
+        System.out.println(pbmTitle + "\n\tbyteNumber = " + byteNumber +
+                           "\n\tshortNumber = " + shortNumber +
+                           "\n\tintNumber = " + intNumber +
+                           "\n\tlongNumber" + longNumber +
+                           "\n\tfloatNumber = " + floatNumber +
+                           "\n\tdoubleNumber = " + doubleNumber + "\n");
+        displayConsole(pbmTitle, byteNumber, shortNumber,
+                       intNumber, longNumber, floatNumber,
+                       doubleNumber);
+
+        keyboardInput.close();
+        secondKeyboardInput.close();
+    } // end of main()
+
+
+    // problem 2(A):
+    /**
+     *
+     * @param pbm      title to present the displayed values
+     * @param bN       byte number
+     * @param sN       short number
+     * @param iN       int number
+     * @param fN       float number
+     * @param dN       double number
+     */
+    public static void displayConsole(String pbm, byte bN, short sN,
+                                      int iN, long lN, float fN,
+                                      double dN) {
         String output = "\n\tbyteNumber = " + bN +
                         "\n\tshortNumber = " + sN +
                         "\n\tintNumber = " + iN +
-                        "\n\tlongNumber = " + lN + 
+                        "\n\tlongNumber = " + lN +
                         "\n\tfloatNumber = " + fN +
-                        "\n\tdoubleNumber = " + dN;
+                        "\n\tdoubleNumber = " + dN + "\n";
         System.out.println("From displayConsole():\n" +
                            pbm + output);
 
-     } // end of displayConsole()
+
+    } // end of displayConsole()
 
  } // end of NumericTypes class
